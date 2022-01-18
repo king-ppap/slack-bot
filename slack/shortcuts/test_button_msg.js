324359cb2ccaee7b68e978bdb156e691ee7197b8
King Ponpawit Paoseng <kingponpawit@gmail.com>
2022-01-18 18:15:39 +0700

    Update test button msg

--- slack/shortcuts/test_button_msg.js
+++ slack/shortcuts/test_button_msg.js
@@ -6,57 +6,60 @@ export default {
   },
   handler: async ({ ack, client }) => {
     await ack();
-    await client.chat.postMessage({
-      channel: 'C02UHFK6EMA',
-      "attachments": [
-        {
-          "color": "#F2C94C",
-          "blocks": [
-            {
-              "type": "divider"
-            },
-            {
-              "type": "header",
-              "text": {
-                "type": "plain_text",
-                "text": `ใบเสนอราคาเลขที่ QT20220100019 ${new Date().toISOString()}`,
-                "emoji": true
-              }
-            },
-            {
-              "type": "divider"
-            },
-            {
-              "type": "actions",
-              "elements": [
-                {
-                  "type": "button",
-                  "style": "primary",
-                  "text": {
-                    "type": "plain_text",
-                    "text": "Approve",
-                    "emoji": true
-                  },
-                  "value": "idOfQt",
-                  "action_id": "QTapprove"
-                },
-                {
-                  "type": "button",
-                  "style": "danger",
-                  "text": {
-                    "type": "plain_text",
-                    "text": "Deny",
-                    "emoji": true
-                  },
-                  "value": "idOfQt",
-                  "action_id": "QTdeny"
+    try {
+      await client.chat.postMessage({
+        channel: process.env.START_CHANEL_ID,
+        "attachments": [
+          {
+            "color": "#F2C94C",
+            "blocks": [
+              {
+                "type": "divider"
+              },
+              {
+                "type": "header",
+                "text": {
+                  "type": "plain_text",
+                  "text": `Test button ${new Date().toISOString()}`,
+                  "emoji": true
                 }
-              ]
-            },
-          ],
-
-        }
-      ]
-    });
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "type": "actions",
+                "elements": [
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "text": {
+                      "type": "plain_text",
+                      "text": "Approve",
+                      "emoji": true
+                    },
+                    "value": "idOfQt",
+                    "action_id": "QTapprove"
+                  },
+                  {
+                    "type": "button",
+                    "style": "danger",
+                    "text": {
+                      "type": "plain_text",
+                      "text": "Deny",
+                      "emoji": true
+                    },
+                    "value": "idOfQt",
+                    "action_id": "QTdeny"
+                  }
+                ]
+              },
+            ],
+          }
+        ]
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 }
