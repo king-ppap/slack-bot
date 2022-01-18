95284f572509ea530c31651d7a38247edaa2b967
King Ponpawit Paoseng <kingponpawit@gmail.com>
2022-01-18 14:09:11 +0700

    Update playing with modal

--- slack.js
+++ slack.js
@@ -2,127 +2,6 @@ import SlackBolt from "@slack/bolt";
 const { App } = SlackBolt;
 
 export default async function start() {// Create a new instance of the WebClient class with the token read from your environment variable
-  //   try {
-  //   // Use the `chat.postMessage` method to send a message from this app
-  //   await web.chat.postMessage({
-  //     channel: 'C02U61FRXSR',
-  //     "attachments": [
-  //       {
-  //         "color": "#F2C94C",
-  //         "blocks": [
-  //           {
-  //             "type": "divider"
-  //           },
-  //           {
-  //             "type": "header",
-  //             "text": {
-  //               "type": "plain_text",
-  //               "text": "ใบเสนอราคาเลขที่ QT20220100019",
-  //               "emoji": true
-  //             }
-  //           },
-  //           {
-  //             "type": "divider"
-  //           },
-  //           {
-  //             "type": "actions",
-  //             "elements": [
-  //               {
-  //                 "type": "button",
-  //                 "style": "primary",
-  //                 "text": {
-  //                   "type": "plain_text",
-  //                   "text": "Approve",
-  //                   "emoji": true
-  //                 },
-  //                 "value": "idOfQt",
-  //                 "action_id": "approve"
-  //               },
-  //               {
-  //                 "type": "button",
-  //                 "style": "danger",
-  //                 "text": {
-  //                   "type": "plain_text",
-  //                   "text": "Deny",
-  //                   "emoji": true
-  //                 },
-  //                 "value": "idOfQt",
-  //                 "action_id": "deny"
-  //               }
-  //             ]
-  //           },
-  //           {
-  //             "type": "divider"
-  //           },
-  //           {
-  //             "type": "section",
-  //             "text": {
-  //               "type": "mrkdwn",
-  //               "text": "เหตุที่ไม่ผ่าน"
-  //             },
-  //             "accessory": {
-  //               "type": "static_select",
-  //               "placeholder": {
-  //                 "type": "plain_text",
-  //                 "text": "Select an item",
-  //                 "emoji": true
-  //               },
-  //               "options": [
-  //                 {
-  //                   "text": {
-  //                     "type": "plain_text",
-  //                     "text": "*this is plain_text text*",
-  //                     "emoji": true
-  //                   },
-  //                   "value": "value-0"
-  //                 },
-  //                 {
-  //                   "text": {
-  //                     "type": "plain_text",
-  //                     "text": "*this is plain_text text*",
-  //                     "emoji": true
-  //                   },
-  //                   "value": "value-1"
-  //                 },
-  //                 {
-  //                   "text": {
-  //                     "type": "plain_text",
-  //                     "text": "*this is plain_text text*",
-  //                     "emoji": true
-  //                   },
-  //                   "value": "value-2"
-  //                 }
-  //               ],
-  //               "action_id": "static_select-action"
-  //             }
-  //           },
-  //           {
-  //             "type": "divider"
-  //           },
-  //           {
-  //             "type": "context",
-  //             "elements": [
-  //               {
-  //                 "type": "image",
-  //                 "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-  //                 "alt_text": "cute cat"
-  //               },
-  //               {
-  //                 "type": "mrkdwn",
-  //                 "text": "# Discord Bot Streaming Now\nBot for Streamer.\nWhen someone have role `streamer`, Voice channel name will change to\n```\n[On Air 🔴] - <Your voice channel name>\n```\n# TODO\n1. ✅ Use role streamer\n2. ⬜ Store data to file or database\n3. 🆗 Refactor!\n4. ⬜ Slash commands\n# Getting Started"
-  //               }
-  //             ]
-  //           }
-  //         ]
-  //       }
-  //     ]
-  //   });
-  //   console.log('Message posted!');
-  // } catch (error) {
-  //   console.log(error);
-  // }
-
-
   const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
@@ -140,6 +19,64 @@ export default async function start() {// Create a new instance of the WebClient
     // Update the message to reflect the action
   });
 
+  app.action('deny', async ({ action, ack, body, client }) => {
+    try {
+      // Call views.open with the built-in client
+      const result = await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: {
+          type: 'modal',
+          // View identifier
+          callback_id: 'view_1',
+          title: {
+            type: 'plain_text',
+            text: 'Modal title'
+          },
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: 'Welcome to a modal with _blocks_'
+              },
+              accessory: {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  text: 'Click me!'
+                },
+                action_id: 'button_abc'
+              }
+            },
+            {
+              type: 'input',
+              block_id: 'input_c',
+              label: {
+                type: 'plain_text',
+                text: 'What are your hopes and dreams?'
+              },
+              element: {
+                type: 'plain_text_input',
+                action_id: 'dreamy_input',
+                multiline: true
+              }
+            }
+          ],
+          submit: {
+            type: 'plain_text',
+            text: 'Submit'
+          }
+        }
+      });
+      ack();
+      console.log(result);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  });
 
 
   try {
